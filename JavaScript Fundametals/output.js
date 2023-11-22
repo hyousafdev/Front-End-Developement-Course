52ce682caf78efd05475d13c165c6416f3e2e42d
Hassan Yousaf <135381130+hyousafdev@users.noreply.github.com>
2023-11-22 23:21:34 +0500

    JavaScript Output ways

---
+++ JavaScript Fundametals/output.js
@@ -0,0 +1,14 @@
+//write to the browser console
+console.log('Hey! from browser console.')
+
+//write to the HTML
+document.write('Hey! from HTML document.')
+
+//output in an alert box
+alert('Hey! from alert box')
+
+//yes/no dialog, returns true/false depending on user click
+confirm('Are you sure?')
+
+//input dialog second argument in the initial value
+prompt('Your age?', '0')
