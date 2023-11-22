8cb073cf9947b1c2b7b2c80ed7cef487a9ab2101
Hassan Yousaf <135381130+hyousafdev@users.noreply.github.com>
2023-11-22 23:41:59 +0500

    JavaScript Data Types

---
+++ JavaScript Fundametals/datatypes.js
@@ -0,0 +1,36 @@
+//Represent as a string
+var greeting = 'Hello, World!';
+
+//Represent as a number
+var age = 25;
+var pi = 3.14;
+
+//Represent as a boolean
+var isStudent = true;
+var hasJob = false;
+
+//Represent as a array
+var colors = ['red', 'green', 'blue'];
+var numbers = [1, 2, 3, 4, 5];
+
+//Represent as a object
+var person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30
+};
+
+//Represent as null value
+var nullValue = null;
+
+//Represent as a declared value but not define
+var undefinedValue;
+
+//Represent as a function
+function add(a, b) {
+    return a + b;
+}
+add(5,5);
+
+//Represent as a unique identifier
+var sym = Symbol('unique');
