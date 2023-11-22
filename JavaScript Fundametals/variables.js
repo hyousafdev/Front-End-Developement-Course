bd125770805f5f438f648c8e65de3f79a1001898
Hassan Yousaf <135381130+hyousafdev@users.noreply.github.com>
2023-11-22 23:28:58 +0500

    JavaScript Variables

---
+++ JavaScript Fundametals/variables.js
@@ -0,0 +1,11 @@
+// Example combining various types of variables
+
+//string
+var firstName = 'Alice';
+var lastName = 'Smith';
+//number
+var age = 28;
+//boolean
+var isStudent = true;
+
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old. Is she a student? ' + isStudent);
