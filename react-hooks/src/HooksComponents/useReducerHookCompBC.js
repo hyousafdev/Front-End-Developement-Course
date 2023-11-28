be6c1b17a277f8a4aff6f80d32e95459e6895cfd
Hassan Yousaf <135381130+hyousafdev@users.noreply.github.com>
2023-11-29 02:34:26 +0500

    useReducer Hook with useContext Hook

---
+++ react-hooks/src/HooksComponents/useReducerHookCompBC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentDE from './useReducerHookCompDE'
+
+function ComponentBC() {
+  return (
+    <div>
+        <ComponentDE />
+    </div>
+  )
+}
+
+export default ComponentBC
---
+++ react-hooks/src/HooksComponents/useReducerHookCompDE.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentEF from './useReducerHookCompEF'
+
+function ComponentDE() {
+  return (
+    <div>
+        <ComponentEF />
+    </div>
+  )
+}
+
+export default ComponentDE
