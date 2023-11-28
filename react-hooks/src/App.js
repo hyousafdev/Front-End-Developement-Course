9967040d1df71f96972f638fae2e69f791764e3c
Hassan Yousaf <135381130+hyousafdev@users.noreply.github.com>
2023-11-29 01:49:54 +0500

    useReducer Hook with Multiple Reducer

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import ReducerCounter from './HooksComponents/useReducerHookSimple';
 import ReducerCounterTComplex from './HooksComponents/useReducerHookComplex';
+import ReducerCounterMultiple from './HooksComponents/useReducerHookMultiple';
 function App() {
   return (
     <div className="App">
       {/* <ReducerCounter /> */}
-      <ReducerCounterTComplex />
+      {/* <ReducerCounterTComplex /> */}
+      <ReducerCounterMultiple />
     </div>
   );
 }
