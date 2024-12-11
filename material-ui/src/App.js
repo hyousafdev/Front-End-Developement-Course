2baca1567b9503dafd75bb6ec17b5cc6152fe6b2
hyousafdev <hassanyousaf.dev@gmail.com>
2024-12-11 16:12:02 +0500

    Hello World and delete the file

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
-      <h2>Learning Material UI I just Started</h2>
+      <h1>Hello World!</h1>
     </div>
   );
 }
