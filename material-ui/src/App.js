343ae9b246e762fa3537c6a21411bd3642946fe1
hyousafdev <hassanyousaf.dev@gmail.com>
2024-12-11 15:59:35 +0500

    Setting up the enviorenment fo Material UI

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey</h1>
-      <h2>Learning Material UI</h2>
+      <h2>Learning Material UI I just Started</h2>
     </div>
   );
 }
